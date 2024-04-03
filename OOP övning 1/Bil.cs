ec507784f2b42d5cd287161c9520e9d89fcc4c81
Elm2006 <isel06002@utb.vaxjo.se>
2024-04-03 10:42:48 +0200

    Add project files.

---
+++ OOP övning 1/Bil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+    class Bil
+    {
+        private string märke;
+        private string model;
+        private string färg;
+        private int årsmodel;
+        private int mil;
+
+        public Bil(string mä, string mo,string f, int å, int mi)
+        {
+        märke = mä;
+        model = mo;
+        färg = f;
+        årsmodel = å;
+        mil = mi;
+        }
+        public void B()
+        {
+        Console.WriteLine(märke);
+        Console.WriteLine(model);
+        Console.WriteLine(färg);
+        Console.WriteLine(årsmodel);
+        Console.WriteLine(mil);
+    }
+    }
---
+++ OOP övning 1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bil p = new Bil("Volvo","V70","Svart",2013,10000);
+        p.B();
+    }
+}
